ed416f77c61f7d815df759c36f11181d1f5e9472
kamlesh <kamlesh_nb@yahoo.com>
2021-04-04 15:43:59 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Just add Git Deployment Option to App Service........
-          Edit <code>src/App.js</code>, Save, Commit & Push to Git Repo to re-deploy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn to Deploy Node, Express & React App to Azure App Service
-        </a>
+        <h1>Azure Hands-On</h1>
       </header>
     </div>
   );
