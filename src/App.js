307c7d0a9618249696fdb1b04d3090a70be62934
kamlesh <kamlesh_nb@yahoo.com>
2021-04-04 14:39:12 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to Deploy Node, Express & React App to Azure App Service
         </a>
       </header>
     </div>
