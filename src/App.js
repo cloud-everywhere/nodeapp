a1dceeef0448b63c7d6881ccecec1899cb903ca2
kamlesh <kamlesh_nb@yahoo.com>
2021-04-04 15:21:26 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        Just add Git Deployment Option to App Service
           Edit <code>src/App.js</code>, Save, Commit & Push to Git Repo to re-deploy
         </p>
         <a
