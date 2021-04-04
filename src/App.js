4d76393273cd1fdb0e1f47b4b4703a72bab9d1f3
kamlesh <kamlesh_nb@yahoo.com>
2021-04-04 14:50:16 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>, Save, Commit & Push to Git Repo to re-deploy
         </p>
         <a
           className="App-link"
